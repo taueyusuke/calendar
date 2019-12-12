7f86ec7bc7c96961d7205dd707d002c60c30ece6
taueyusuke <etoauve@icloud.com>
2019-12-12 03:02:39 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'conferencerooms#index'
-  get '/shcedules', to: 'scheduls#index'
   get '/users/:id', to: 'users#show', as: 'user'
   get '/conferencerooms', to: 'conferencerooms#show'
-  get '/conferencerooms/:id', to: 'conferencerooms#show', as: 'conferenceroom'
+  
   resources :conferencerooms do
    resources :users, only: %i(new create)
    resources :schedules
